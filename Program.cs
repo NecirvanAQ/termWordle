306093ae24c3744e4751998dd45f9eb0d993a913
Necirvan <AQ231029@aquinas.ac.uk>
2023-09-22 10:25:08 +0100

    added colors.net

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colors.Net;
+using static Colors.Net.StringStaticMethods;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ColoredConsole.WriteLine($"{Red("red")}, {Green("green")}, {Blue("blue")}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
