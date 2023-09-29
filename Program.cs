93746364a282b9718ac28f5653b3f11e59da8fc6
Necirvan <AQ231029@aquinas.ac.uk>
2023-09-29 20:26:33 +0100

    fixed

--- Program.cs
+++ Program.cs
@@ -1,24 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Colors.Net;
-using static Colors.Net.StringStaticMethods;
-
-namespace ConsoleApp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                ColoredConsole.WriteLine($"{Red("red")}, {Green("green")}, {Blue("blue")}");
-            }
-
-
-            Console.ReadKey();
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
