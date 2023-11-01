0a2472dea57aaaed7e40e5e58cb52e2c0b7acc33
Necirvan <AQ231029@aquinas.ac.uk>
2023-11-01 15:57:09 +0000

    more

--- Program.cs
+++ Program.cs
@@ -8,11 +8,13 @@ using static Colors.Net.StringStaticMethods;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
 
-class Column
+class Row
 {
-    List<string> letters = new List<string> {"" , "", "", "", ""};
+    public List<string> letters = new List<string> {"_" , "_", "_", "_", "_"};
 
-    static bool RealWord(List<string> letters)
+    public string guess = "";
+
+    public static bool RealWord(List<string> letters)
     {
         StreamReader sr = new StreamReader("words.txt");
         while(!sr.EndOfStream)
@@ -26,12 +28,47 @@ class Column
         }
         return false;
     }
+
 }
 class Program
 {
     static void Main(string[] args)
     {
-        
+        Row one = new Row(); Row two = new Row(); Row three = new Row(); Row four = new Row(); Row five = new Row();
+        List<Row> rows = new List<Row> { one, two, three, four, five };
+        foreach (Row row in rows)
+        {
+            menu(one, two, three, four, five);
+
+            row.guess = GetGuess(row.guess);
+
+            Console.ReadKey();
+        }
+    }   
+
+    static void menu(Row one, Row two, Row three, Row four, Row five)
+    {
+        Console.Clear();
+        Console.WriteLine($"\nTERM WORDLE\n");
+        PrintRow(one.letters);
+        PrintRow(two.letters);
+        PrintRow(three.letters);
+        PrintRow(four.letters);
+        PrintRow(five.letters);
     }
 
+    public static void PrintRow(List<string> letters)
+    {
+        foreach (string letter in letters)
+        {
+            Console.Write(letter);
+        }
+        Console.WriteLine();
+    }
+
+    public static string GetGuess(string guess)
+    {
+        guess = Console.ReadLine();
+        return guess;
+    }
 }
