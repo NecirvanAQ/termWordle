ca9d7274edfc1bbe1c445826572b86f01e3f54a4
Necirvan <AQ231029@aquinas.ac.uk>
2023-11-02 16:31:42 +0000

    fully functioning (without colours)

--- Program.cs
+++ Program.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Colors.Net;
-using static Colors.Net.StringStaticMethods;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics.Tracing;
 
 class Row
 {
@@ -14,14 +13,14 @@ class Row
 
     public string guess = "";
 
-    public static bool RealWord(List<string> letters)
+    public static bool RealWord(string guess)
     {
         StreamReader sr = new StreamReader("words.txt");
         while(!sr.EndOfStream)
         {
             string line = sr.ReadLine();
 
-            if (line == Convert.ToString(letters))
+            if (line == guess)
             {
                 return true;
             }
@@ -29,21 +28,49 @@ class Row
         return false;
     }
 
+    public static void UpdateLetters(string guess, List<string> letters)
+    {
+        try
+        {
+            for (int i = 0; i != 5; i++)
+            {
+                letters[i] = Convert.ToString(guess[i]);
+            }
+        } catch (Exception e) { };
+    }
+
 }
 class Program
 {
     static void Main(string[] args)
     {
+        Random rnd = new Random();
+
+        string answer = SelectWord(rnd);
+        List<string> answerList = new List<string> {$"{answer[0]}", $"{answer[1]}", $"{answer[2]}", $"{answer[3]}", $"{answer[4]}" };
+
         Row one = new Row(); Row two = new Row(); Row three = new Row(); Row four = new Row(); Row five = new Row();
         List<Row> rows = new List<Row> { one, two, three, four, five };
+
         foreach (Row row in rows)
         {
             menu(one, two, three, four, five);
+            Console.WriteLine(answer);
 
-            row.guess = GetGuess(row.guess);
+            while (Row.RealWord(row.guess) != true)
+            {
+                GetGuess(ref row.guess);
+                Row.UpdateLetters(row.guess, row.letters);
+            }
 
-            Console.ReadKey();
+            if (row.guess == answer)
+            {
+                menu(one, two, three, four, five);
+                Console.WriteLine("You won!");
+                break;
+            }
         }
+        Console.ReadKey();
     }   
 
     static void menu(Row one, Row two, Row three, Row four, Row five)
@@ -66,9 +93,29 @@ class Program
         Console.WriteLine();
     }
 
-    public static string GetGuess(string guess)
+    public static string GetGuess(ref string guess)
     {
         guess = Console.ReadLine();
         return guess;
+    }
+
+    public static string SelectWord(Random rnd)
+    {
+        StreamReader sr = new StreamReader("words.txt");
+        int t = rnd.Next(1, 1000);
+        int i = 0;
+        while (!sr.EndOfStream)
+        {
+            string line = sr.ReadLine();
+
+            if (i == t)
+            {
+                sr.Close();
+                return line;
+            }
+            i++;
+        }
+        sr.Close();
+        return "error";
     }
 }
